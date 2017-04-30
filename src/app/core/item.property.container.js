71e40a97324ee11b395c9831f175411453a4a177
Ruslan Pedora <ruslan.pedora.gr@gmail.com>
2017-05-01 00:26:33 +0300

    to upload to github

---
+++ src/app/core/item.property.container.js
@@ -0,0 +1,10 @@
+"use strict";
+var ItemPropertyContainer = (function () {
+    function ItemPropertyContainer(itemPropertry) {
+        this.itemProperty = itemPropertry;
+        this.itemPropertyList = [];
+    }
+    return ItemPropertyContainer;
+}());
+exports.ItemPropertyContainer = ItemPropertyContainer;
+//# sourceMappingURL=item.property.container.js.map
